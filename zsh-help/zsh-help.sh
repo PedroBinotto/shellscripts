44de6508e61db0ed8bcd36f88e7d6288e9098e06
Pedro Binotto <pedrosantibinotto@gmail.com>
2022-03-11 17:55:24 -0300

    shellhelp

---
+++ zsh-help/zsh-help.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cat << EOF
+Ctrl-f :: sessionizer
+
+Alt-a       -> sessionizer :: ~/Projetos/bridge/sgtes-residencias/frontend
+Alt-s       -> sessionizer ::               ~/Projetos/bridge/pec/frontend
+Alt-j       -> sessionizer ::                           ~/Projetos/scripts
+Alt-k       -> sessionizer ::                                  ~/.dotfiles
+
+zshconfig   -> \$EDITOR     ::                                     ~/.zshrc
+i3config    -> \$EDITOR     ::                          ~/.config/i3/config
+kittyconfig -> \$EDITOR     ::                   ~/.config/kitty/kitty.conf
+polyconfig  -> \$EDITOR     ::                     ~/.config/polybar/config
+vimconfig   -> \$EDITOR     ::                      ~/.config/nvim/init.vim
+piconfig    -> \$EDITOR     ::                   ~/.config/picom/picom.conf
+emacsconfig -> \$EDITOR     ::                                     ~/.emacs
+music       -> ncmpcpp
+\:q         -> exit
+EOF
+
